ddcb1f59e79680af4f662c1e316d5a8bf005a3e1
Pablo <pablo.quinci@gmail.com>
2024-08-29 16:41:44 -0300

    Primer commit

---
+++ src/main/java/com/possumus/apiconverter/ApiConverterApplication.java
@@ -0,0 +1,13 @@
+package com.possumus.apiconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiConverterApplication.class, args);
+    }
+
+}
